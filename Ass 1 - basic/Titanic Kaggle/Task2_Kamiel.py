d441459d124f4dff7b6b3cbfe13357e5941dc013
Kamiel <k.gulpen@hotmail.nl>
2021-04-12 10:33:34 +0200

    m

--- Ass 1 - basic/Titanic Kaggle/Task2_Kamiel.py
+++ Ass 1 - basic/Titanic Kaggle/Task2_Kamiel.py
@@ -17,29 +17,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 from sklearn.ensemble import AdaBoostClassifier
 
-def cleaner(df):
-    df["Binary_Sex"] = df["Sex"]
-    print(df["Binary_Sex"])
-    df["Binary_Sex"] = df["Binary_Sex"].replace(["female", "male"], [1, 0])
-    enc = OneHotEncoder(handle_unknown='ignore')# passing bridge-types-cat column (label encoded values of bridge_types)
-
-    enc_df = pd.DataFrame(enc.fit_transform(df[["Pclass"]]).toarray())# merge with main df bridge_df on key values
-    df = df.join(enc_df)
-    for i in range(3):
-        df = df.rename(columns={i:f"C{i+1}"})
-
-    
-
-    df["Cabin_Binary"] = df["Cabin"]
-    for i in df["Cabin_Binary"]:
-
-        if type(i) == str:
-            df["Cabin_Binary"] = df["Cabin_Binary"].replace(i,1)
-        else:
-            df["Cabin_Binary"] = df["Cabin_Binary"].replace(i,0)
-    return df 
-
-
 df = pd.read_csv("Ass 1 - basic/Titanic Kaggle/Data/train.csv")
 df = cleaner(df)
 print(df.columns)
