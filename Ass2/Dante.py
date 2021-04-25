1aa0edfec73a7efdbdcf67d76f8f5e1c452c2390
DCCdelang <33282294+DCCdelang@users.noreply.github.com>
2021-04-25 12:50:21 +0200

    change

--- Ass2/Dante.py
+++ Ass2/Dante.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import numpy as np
 
+# Small subset
+# df = pd.read_csv("Ass2/Data/training_set_VU_DM.csv")
+# df.head(10000).to_csv("training_head.csv")
 
+df = pd.read_csv("Ass2/training_head.csv")
+# print(df.columns)
+# print(df.nunique())
 
-df = pd.read_csv("Ass2/Data/training_set_VU_DM.csv")
+"""
+TODO:
 
-print(df.columns)
-print(df.nunique())
-
+Variables with missing 
+"""
