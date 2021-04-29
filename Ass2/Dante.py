4d6a72edaac07d74cbfe4ce3fcaca317a319d758
Kamiel <k.gulpen@hotmail.nl>
2021-04-29 10:52:43 +0200

    m

---
+++ Ass2/Dante.py
@@ -0,0 +1,173 @@
+import pandas as pd
+import numpy as np
+from numba import jit
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+
+# Small subset
+# df = pd.read_csv("Ass2/Data/training_set_VU_DM.csv")
+# df.head(10000).to_csv("training_head.csv")
+
+df = pd.read_csv("Ass2/training_head.csv")
+# print(df.columns)
+# print(df.nunique())
+
+"""
+TODO:
+
+Variables with missing 
+"""
+# print(df.dtypes)
+
+# Filters rows with booking boolean is 1
+def booking_Filter(df):
+    df = df.loc[df["booking_bool"] == 1]
+    return df
+
+# Filters rows with clicking boolean is 1
+def clicking_Filter(df):
+    df = df.loc[df["click_bool"] == 1]
+    return df
+
+# Plot correlation plot full df
+def plot_corr(df):
+    _, ax = plt.subplots(figsize=(10, 8))
+    corr = df.corr()
+    sns.heatmap(corr, mask=np.zeros_like(corr, dtype=np.bool), cmap=sns.diverging_palette(220, 10, as_cmap=True),square=True, ax=ax)
+    plt.show()
+
+# Make extra colomns time and hour, plot possibility for hour distribution
+def extract_date_time(df, plot = False):
+    df['date'] = pd.to_datetime(df['date_time'], format="%Y-%d-%m %H:%M:%S",infer_datetime_format=True)
+
+    df["time"] = df["date"].dt.time
+    df['hour'] = df['date'].dt.hour
+
+    if plot == True:
+        sns.histplot(df["hour"],bins=24)
+        plt.show()
+    return df
+
+# Scatterplot for prop location colomns, important features!
+def prop_loc_plot(df):
+    sns.scatterplot(x=df["prop_location_score1"], y=df["prop_location_score2"])
+    plt.show()
+
+# Plot position on list with respect to clicking boolean
+def position_click(df):
+    df_book = booking_Filter(df)
+    df_click = clicking_Filter(df)
+
+    plt.hist([df_book["position"],df_click["position"]],color=["r","b"], label=["Booking","Clicking"],alpha=0.5,bins=37)
+    plt.legend()
+    plt.show()
+    print(set(df_click["position"]))
+
+def summary(df):
+    print(df.describe)
+    print(df.nunique())
+
+# Results per query
+def hotels_per_id(df):
+    group_id = df.groupby(["srch_id"]).size()
+    print("Hotels per srch_id \nMean:", group_id.mean(),"\nStd:", group_id.std())
+
+# Gross price divided by days of stay
+def price_per_day(df):
+    # df = booking_Filter(df).copy()
+    df["gross_bookings_per_day"] = df["gross_bookings_usd"]/df["srch_length_of_stay"]
+    df["price_per_day"] = df["price_usd"]/df["srch_length_of_stay"]
+    return df
+
+
+# Convert to usd prices and difference
+def exp_historical_price_dif(df):
+    df["historical_price"] = np.exp(df["prop_log_historical_price"])
+    df["hist_price_dif"] = df["historical_price"] - df["price_usd"]
+    return df
+
+# Convert to log prices and difference
+def log_historical_price_dif(df):
+    df["log_price_usd"] = np.log(df["price_usd"])
+    df["log_hist_price_dif"] = df["prop_log_historical_price"] - df["log_price_usd"]
+    return df
+
+def starrating_diff(df):
+    df["starrating_diff"]=np.abs(df["visitor_hist_starrating"]-df["prop_starrating"])
+
+# position_click(df)
+# prop_loc_plot(df)
+# # exp_historical_price_dif(df)
+# plot_corr(df)
+# print(df.head())
+
+def prop_quality(df):
+    df["count"] = 1
+    df = df.join(df.groupby(["prop_id"])["booking_bool"].sum(), on="prop_id",rsuffix="_tot")
+    df = df.join(df.groupby(["prop_id"])["count"].sum(), on="prop_id",rsuffix="_tot")
+    df["prob_book"] = df["booking_bool_tot"]/df["count_tot"]
+    df.drop(["count"],axis=1)
+    
+    # print(set(df["prob_book"]))
+    return df
+
+# prop_quality(df)
+
+"""Average functies maken! Worden prop_ids duidelijker"""
+
+# Function to average out numerical values per property, can be done in combination with test set. Should be done at beginning?!
+def averages_per_prop(df):
+    df["count"] = 1
+    df = df.join(df.groupby(["prop_id"])["prop_starrating"].mean(), on="prop_id",rsuffix="_avg")
+
+    df = df.join(df.groupby(["prop_id"])["prop_review_score"].mean(), on="prop_id",rsuffix="_avg")
+
+    df = df.join(df.groupby(["prop_id"])["prop_location_score1"].mean(), on="prop_id",rsuffix="_avg")
+
+    df = df.join(df.groupby(["prop_id"])["prop_location_score2"].mean(), on="prop_id",rsuffix="_avg")
+
+    df = df.join(df.groupby(["prop_id"])["prop_log_historical_price"].mean(), on="prop_id",rsuffix="_avg")
+
+    df = df.join(df.groupby(["prop_id"])["price_usd"].mean(), on="prop_id",rsuffix="_avg")
+
+    df.drop(["count"],axis=1)
+    return df
+
+def std_per_prop(df):
+    df["count"] = 1
+    df = df.join(df.groupby(["prop_id"])["prop_starrating"].std(), on="prop_id",rsuffix="_std")
+
+    df = df.join(df.groupby(["prop_id"])["prop_review_score"].std(), on="prop_id",rsuffix="_std")
+
+    df = df.join(df.groupby(["prop_id"])["prop_location_score1"].std(), on="prop_id",rsuffix="_std")
+
+    df = df.join(df.groupby(["prop_id"])["prop_location_score2"].std(), on="prop_id",rsuffix="_std")
+
+    df = df.join(df.groupby(["prop_id"])["prop_log_historical_price"].std(), on="prop_id",rsuffix="_std")
+
+    df = df.join(df.groupby(["prop_id"])["price_usd"].std(), on="prop_id",rsuffix="_std")
+
+    df.drop(["count"],axis=1)
+    return df
+
+def median_per_prop(df):
+    df["count"] = 1
+    df = df.join(df.groupby(["prop_id"])["prop_starrating"].median(), on="prop_id",rsuffix="_median")
+
+    df = df.join(df.groupby(["prop_id"])["prop_review_score"].median(), on="prop_id",rsuffix="_median")
+
+    df = df.join(df.groupby(["prop_id"])["prop_location_score1"].median(), on="prop_id",rsuffix="_median")
+
+    df = df.join(df.groupby(["prop_id"])["prop_location_score2"].median(), on="prop_id",rsuffix="_median")
+
+    df = df.join(df.groupby(["prop_id"])["prop_log_historical_price"].median(), on="prop_id",rsuffix="_median")
+
+    df = df.join(df.groupby(["prop_id"])["price_usd"].median(), on="prop_id",rsuffix="_median")
+
+    df.drop(["count"],axis=1)
+    return df
+
+df = averages_per_prop(df)
+print(df.head())
--- Ass2/Kamiel.py
+++ Ass2/Kamiel.py
@@ -1,20 +1,142 @@
 import pandas as pd
-import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.inspection import permutation_importance
+from sklearn.ensemble import GradientBoostingRegressor,GradientBoostingClassifier
+from sklearn.metrics import ndcg_score
 
-df = pd.read_csv('Ass2/Data/training_set_VU_DM.csv')
-df2 = pd.read_csv('Ass2/Data/clicked_data.csv')
-df3 = df
-important = list(df2['srch_id'])
+def make_files():
 
+    df = pd.read_csv('Data/clicked_data.csv')
 
+    df = df.tail(100000)
+    df.to_csv('Data/train_data.csv', index=False)
 
-for i in range(1, 332785 + 1):
-    if i not in important:
-        df3 = df[df.srch_id != i]
-    else:
-        important.remove(i)
-    if i %1000 == 0:
-        print(i)
+def add_values(df, name):
+
+    values = []
+    for i in range(len(df['ra'])):
+        value = 0
+        if df.iloc[i]['click_bool'] != 0:
+            value +=1
+        if df.iloc[i]['booking_bool'] != 0:
+            value +=4
+        values.append(value)
+
+    df['value'] = values
+    df.to_csv('Data/test_data.csv', index=False)
+       
+def train_model():
+    train = pd.read_csv('Data/train_data.csv')
+    train = train.fillna(0)
+
+    features = ['price_usd', 'prop_review_score', 'prop_starrating','promotion_flag', 'prop_starrating','srch_saturday_night_bool','srch_room_count','srch_children_count']
+    X = train[features]
+    y = train["value"]  
+    reg = GradientBoostingRegressor(n_estimators=100, learning_rate=1.0,
+    max_depth=1, random_state=0)
+    reg = reg.fit(X, y)
+    return reg
+
+def test_model(reg):
     
-df3.to_csv('cool.csv')
+    test = pd.read_csv('Data/test_data.csv')
+    
+    test = test.fillna(0)
+    scores = []
+    ids = list(set(test['ra']))
 
+    for i in ids:
+        test1 = test.loc[test['ra'] == i]
+        # print(test)
+        features = ['price_usd', 'prop_review_score', 'prop_starrating','promotion_flag', 'prop_starrating','srch_saturday_night_bool','srch_room_count','srch_children_count']
+        X = test1[features]
+        y = test1["value"]  
+        
+        predictions = reg.predict(X)
+
+        true = np.asarray([y])
+        predict = np.asarray([list(predictions)])
+        score = ndcg_score(true, predict)
+        scores.append(score)
+    print('hallo')
+    print(np.mean(scores))
+
+def feature_importance():
+
+    train = pd.read_csv('Data/train_data.csv')
+    train = train.fillna(0)
+    feat = list(train.columns)
+
+    feat.remove('position')
+    feat.remove('click_bool')
+    feat.remove('gross_bookings_usd')
+    feat.remove('value')
+    feat.remove('booking_bool')
+    feat.remove('srch_saturday_night_bool')
+    feat.remove('srch_room_count')
+    feat.remove('srch_children_count')
+        
+  
+    features = feat[3:-1]
+    X = train[features]
+    y = train["value"]  
+
+    reg = GradientBoostingRegressor(n_estimators=100, learning_rate=1.0,
+    max_depth=1, random_state=0)
+
+    reg = reg.fit(X, y)
+    feature_importance = reg.feature_importances_
+    sorted_idx = np.argsort(feature_importance)
+    pos = np.arange(sorted_idx.shape[0]) + .5
+    fig = plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.barh(pos, feature_importance[sorted_idx], align='center')
+    plt.yticks(pos, np.array(feat)[sorted_idx])
+    plt.title('Feature Importance (MDI)')
+
+    result = permutation_importance(reg, X, y, n_repeats=10,
+                                    random_state=42, n_jobs=2)
+    sorted_idx = result.importances_mean.argsort()
+    plt.subplot(1, 2, 2)
+    plt.boxplot(result.importances[sorted_idx].T,
+                vert=False, labels=np.array(feat)[sorted_idx])
+    plt.title("Permutation Importance (test set)")
+    fig.tight_layout()
+    plt.show()
+
+def make_submission_file():
+    train = pd.read_csv('Data/clicked_data.csv')
+    train = train.fillna(0)
+
+    features = ['price_usd', 'prop_review_score', 'prop_starrating','promotion_flag', 'prop_starrating','srch_saturday_night_bool']
+    X = train[features]
+    y = train["value"]  
+    reg = GradientBoostingRegressor(n_estimators=100, learning_rate=1.0,
+    max_depth=1, random_state=0)
+    reg = reg.fit(X, y)
+    
+    test = pd.read_csv('Data/test_set_VU_DM.csv')
+    test = test.fillna(0)
+    df_sub = test
+    
+    X = test[features] 
+    predictions = reg.predict(X)
+
+    df_sub['predicted_values'] = predictions
+
+    df_sub = df_sub.sort_values(['srch_id', 'predicted_values'], ascending=[True, False])
+    
+    df_sub = df_sub[['srch_id', 'prop_id']]
+
+    df_sub.to_csv('Data/submission1.csv', index=False)
+
+    
+
+# feature_importance()
+# df = pd.read_csv('Data/test_data.csv')
+# add_values(df, 'Data/test_data.csv')
+# df = pd.read_csv('Data/train_data.csv')
+# add_values(df, 'Data/train_data.csv')
+test_model(train_model())
+# make_files()
